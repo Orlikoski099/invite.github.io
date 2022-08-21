2531c23260fd69e152e80eeef35d85868c654b8a
Lucas Orlikoski <77631021+Orlikoski099@users.noreply.github.com>
2022-08-20 21:37:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function MovePosition(){
+    marginLeft = parseInt (Math.random() * 1000);
+    marginTop = parseInt (Math.random() * 1000);
+    const runningButton = document.getElementById("runningButton");  
+    runningButton.style.position="fixed"
+    runningButton.style.left = marginLeft +'px';
+    runningButton.style.marginTop = marginTop+'px';
+}
+function autoCopy(){
+    navigator.clipboard.writeText("Oi, vamo onde?");
+    alert("De um ctrl + v no meu chat!")
+    document.getElementById("body").style.backgroundImage = 'url("https://imagepng.org/coracao/")';
+}
